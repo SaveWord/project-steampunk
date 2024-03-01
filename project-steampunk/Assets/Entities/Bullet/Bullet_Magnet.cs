0ec55e765565a1fd22d29ae5e0d1c91b5649fd30
deviantsquirrel <nikitinakatarina111@gmail.com>
2024-03-01 23:02:16 +0300

    magnet_bullet

--- project-steampunk/Assets/Entities/Bullet/Bullet_Magnet.cs
+++ project-steampunk/Assets/Entities/Bullet/Bullet_Magnet.cs
@@ -1,6 +1,7 @@
 using Enemies.Bullets;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet_Magnet : Bullet
@@ -8,47 +9,31 @@ public class Bullet_Magnet : Bullet
     [SerializeField] private float MagnetRad;
     [SerializeField] private float TurnAngle;
     private float distance;
-    private float rotationTime;
-    private bool go = false;
-    private Vector3 continueDirection;
+    protected float _turntime;
 
     public override void OnFly()
     {
         _timeOnFly += Time.deltaTime;
+        _turntime += Time.deltaTime;
         if (_timeOnFly >= _lifeTime) SelfDestroy();
         if (targetObject != null)
         {
             distance = Vector3.Distance(gameObject.transform.position, targetObject.transform.position);
-            
-            if (distance < MagnetRad)
+            if (distance < MagnetRad && _turntime > 0.5f)
             {
-                go = false;
-                Debug.Log("affcet");
-                if((targetObject.transform.position - transform.position).normalized != (lastKnownPosition - transform.position).normalized)
+                Vector3 targetDirection = (targetObject.transform.position - transform.position).normalized;
+                Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+                float angleDifference = Quaternion.Angle(transform.rotation, targetRotation);
+                if (angleDifference <= TurnAngle)
                 {
-                    float angle = Vector3.Angle((targetObject.transform.position - transform.position).normalized, (lastKnownPosition - transform.position).normalized);
-                    Debug.Log("Angle between vectors: " + angle);
-                    if(angle > 10f) {
-
-                    
-                    }
+                    transform.rotation = targetRotation;
                 }
-                rotationTime += Time.deltaTime;
-                Vector3 direction = targetObject.transform.position - transform.position;
-                Quaternion targetRotation = Quaternion.LookRotation(direction);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, TurnAngle * rotationTime);
-            }
-            else
-            {
-                if (!go)
+                else
                 {
-                    rotationTime = 0f;
-                    continueDirection = (lastKnownPosition - transform.position).normalized;
-                    go = true;
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, TurnAngle);
                 }
-                Debug.Log(continueDirection);
-                transform.position += continueDirection * _speed * Time.deltaTime;
-                Debug.Log(transform.position);
+                _rBody.velocity = transform.forward * _speed;
+                _turntime = 0;
             }
         }
         else
