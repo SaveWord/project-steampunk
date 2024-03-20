67d5d83c548d7ceb3c675a111db4f36928d81882
RomanS211 <serbinroman5819@gmail.com>
2024-03-20 13:55:20 +0300

    20.03.2024_Level1Blocking0-Level3Blocking03_Save

--- project-steampunk/Assets/Art/Environment/Scripts/BigWheel.cs
+++ project-steampunk/Assets/Art/Environment/Scripts/BigWheel.cs
@@ -7,7 +7,21 @@ public class BigWheel : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
-            other.gameObject.transform.SetParent(transform);
+        {
+            other.gameObject.transform.SetParent(transform, true);
+            
+        }
+            
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 playerrotate = other.gameObject.transform.rotation.eulerAngles;
+            playerrotate.z = 0;
+            playerrotate.x = 0;
+            other.gameObject.transform.rotation = Quaternion.Euler(playerrotate);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
