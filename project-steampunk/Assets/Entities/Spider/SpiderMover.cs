fb08e4db6141b702fe480be4134435ae344d3a2d
deviantsquirrel <nikitinakatarina111@gmail.com>
2024-03-24 10:51:09 +0300

    little spider

--- project-steampunk/Assets/Entities/Spider/SpiderMover.cs
+++ project-steampunk/Assets/Entities/Spider/SpiderMover.cs
@@ -1,17 +1,18 @@
-using Enemies.AntMoves;
+using Enemies.SpiderStates;
 using System;
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace Enemies
 {
-    public class AntMover : MonoBehaviour
+    public class SpiderMover : MonoBehaviour
     {
         [Header("Basics")]
         [SerializeField] private float _moveSpeed = 5f;
-        [SerializeField] private Dash _dash;
+        //[SerializeField] private Dash _dash;
 
         private NavMeshAgent _nMeshAgent;
         ///[SerializeField] 
@@ -28,29 +29,36 @@ namespace Enemies
         private float distance;
         private Vector3 targetPosition;
         [SerializeField] private NavMeshAgent navMeshAgent;
-        [SerializeField] private bool ReverceDash;
-        [SerializeField] private float DashForwardReactDist;
-        [SerializeField] private float DashBackwardReactDist;
+        [SerializeField] private float DashDistance;
+        [SerializeField] private float _damageCloseCombat;
+        [SerializeField] private float _damageJump;
+        [SerializeField] private float dashoffset;
+        [SerializeField] private float Cooldowntime;
+        [SerializeField] private float JumpCooldowntime;
+
+        private bool damagecooldown = false;
+        private bool jumpdamagecooldown = false;
 
         public void MoveToTarget(ITarget target)
         {
+
             if (_nMeshAgent.enabled)
             {
                 _nMeshAgent.SetDestination(target.GetPosition());
 
-                
+
 
                 _animator.SetBool("isRunning", true);
 
-                _controlarrow.ChangeColorToGray();
-                _controlarrow.Show();
+                //_controlarrow.ChangeColorToGray();
+                //_controlarrow.Show();
             }
             else
             {
                 _animator.SetBool("isRunning", false);
-                _controlarrow.Hide();
+                //_controlarrow.Hide();
             }
-                
+
 
         }
 
@@ -63,11 +71,17 @@ namespace Enemies
             {
                 return;
             }
-
-            bool shouldDash = (!ReverceDash && distance > DashForwardReactDist) || (ReverceDash && distance < DashBackwardReactDist);
-            if (shouldDash)
+            if (jumpdamagecooldown)
             {
+                return;
+            }
+            Debug.Log("DashDistance + dashoffset" + (DashDistance + dashoffset));
+            Debug.Log("DashDistance - dashoffset" + (DashDistance - dashoffset));
+            if (distance < (DashDistance + dashoffset) && distance > (DashDistance - dashoffset))
+            {
+                Debug.Log("ready to jump");
                 StartCoroutine(GoDash(dashDuration));
+                StartCoroutine(JumpCooldown(JumpCooldowntime));
             }
 
         }
@@ -76,9 +90,8 @@ namespace Enemies
             canDash = false;
             ToggleControlToRBody();
             Vector3 dashDirection = (targetPosition - transform.position).normalized;
-            if(ReverceDash) { dashDirection = -dashDirection; }
 
-           
+
             _rBody.AddForce(Vector3.up * dashForce, ForceMode.Impulse);
             _rBody.AddForce(dashDirection * dashForce, ForceMode.Impulse);
 
@@ -86,7 +99,7 @@ namespace Enemies
             ToggleControlToNavMesh();
             yield return new WaitForSeconds(dashCooldown);
             canDash = true;
-            
+
         }
         private void ToggleControlToRBody()
         {
@@ -112,17 +125,18 @@ namespace Enemies
         {
             _nMeshAgent = GetComponent<NavMeshAgent>();
             _rBody = GetComponent<Rigidbody>();
-            
-            _dash = new Dash(_rBody, this);
-            _controlarrow = GetComponent<controlarrow>();
+
+            //_dash = new Dash(_rBody, this);
+            //_controlarrow = GetComponent<controlarrow>();
 
             _animator = GetComponentInChildren<Animator>();
-            
+            _moveSpeed = GameObject.FindWithTag("Player").GetComponent<PlayerMove>().GetSpeed() * 0.9f;
+
         }
 
         private void OnDrawGizmos()
         {
-            if(canDash)
+            if (canDash)
             {
                 Gizmos.color = Color.yellow;
                 Gizmos.DrawCube(transform.position + Vector3.up * 10, Vector3.one * 3f);
@@ -132,9 +146,44 @@ namespace Enemies
                 Gizmos.color = Color.magenta;
                 Gizmos.DrawCube(transform.position + Vector3.up * 10, Vector3.one * 3f);
             }
-                
+
         }
 
+        protected void OnCollisionEnter(Collision collision)
+        {
+            
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("hit player");
+                HpHandler damageScript = collision.gameObject.GetComponent<HpHandler>();
+                if (canDash && !damagecooldown)
+                {
+                    damageScript.TakeDamage(_damageCloseCombat);
+                    StartCoroutine(Cooldown(Cooldowntime));
+                }
+                else if (!damagecooldown) { 
+                    damageScript.TakeDamage(_damageJump);
+                    StartCoroutine(Cooldown(Cooldowntime));
+                }
+                
+            }
+        }
+        IEnumerator Cooldown(float time)
+        {
+            damagecooldown = true;
+
+            yield return new WaitForSeconds(time);
+            damagecooldown = false;
+
+        }
+        IEnumerator JumpCooldown(float time)
+        {
+            jumpdamagecooldown = true;
+
+            yield return new WaitForSeconds(time);
+            jumpdamagecooldown = false;
+
+        }
     }
 }
 
---
+++ project-steampunk/Assets/Entities/Spider/States/Chase.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemies.SpiderStates
+{
+    public class Chase : IState
+    {
+        private NavMeshAgent _nMeshAgent;
+        private SpiderMover _spiderMover;
+        private ITargetDetector _targetHolder;
+
+        public Chase(NavMeshAgent nMeshAgent, SpiderMover spiderMover, ITargetDetector targetHolder)
+        {
+            _nMeshAgent = nMeshAgent;
+            _spiderMover = spiderMover;
+            _targetHolder = targetHolder;
+        }
+
+        public Color GizmoColor()
+        {
+            return Color.yellow;
+        }
+
+        public void Tick()
+        {
+            var target = _targetHolder.GetTarget();
+
+            if (target != null)
+            {
+                _spiderMover.MoveToTarget(target);
+                _spiderMover.Dash(target);
+            }
+        }
+
+        public void OnEnter()
+        {
+            return;
+        }
+
+        public void OnExit()
+        {
+            return;
+        }
+
+    }
+}
