6efeb7032b4e0c7d6b2c352141b3b7084278490c
Denis <denis.kryuchkov.2000@mail.ru>
2024-03-25 21:26:32 +0300

    delete old animation, add in player prefab 3 variant mechanic weapon

--- project-steampunk/Assets/Code/Scripts/Shield/ShieldHealth.cs
+++ project-steampunk/Assets/Code/Scripts/Shield/ShieldHealth.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class ShieldHealth : MonoBehaviour, IShield
-{ 
+{
     [SerializeField] private float disolveRate;
     [SerializeField] private float refreshRate;
     private Material m_Material;
@@ -19,21 +21,34 @@ public class ShieldHealth : MonoBehaviour, IShield
     }
     IEnumerator ShieldImpulseVFX()
     {
+
+        m_Material.SetFloat("_FreshelPower", 2.2f);
+        m_Material.SetVector("_VertexAmount", new Vector3(0.05f, 0.05f, 0.05f));
+        yield return new WaitForSeconds(refreshRate);
+        m_Material.SetFloat("_FreshelPower", 2f);
+        m_Material.SetVector("_VertexAmount", new Vector3(0, 0, 0));
+
+        /*
         float counter = 0;
-        while(m_Material.GetFloat("_FreshelPower") <= 2)
+        while(m_Material.GetFloat("_FreshelPower") <= 2 &&
+            m_Material.GetFloat("_FreshelPower") >= 1.8f)
         {
             counter += 0.2f;
             m_Material.SetFloat("_FreshelPower", counter);
             m_Material.SetVector("_VertexAmount",new Vector3(0.05f,0.05f,0.05f));
             yield return new WaitForSeconds(refreshRate);
         }
-        while(m_Material.GetFloat("_FreshelPower") >= 2)
+        while(m_Material.GetFloat("_FreshelPower") >= 2 &&
+            m_Material.GetFloat("_FreshelPower") <= 2.2f)
         {
             counter -= 0.2f;
             m_Material.SetFloat("_FreshelPower", counter);
             m_Material.SetVector("_VertexAmount", new Vector3(0, 0, 0));
             yield return new WaitForSeconds(refreshRate);
         }
+       */
+
+
     }
     public void ShieldDestroy()
     {
