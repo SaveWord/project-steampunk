4bb9f1d667426aaa07a5fadcc1c4b43d7e32cd34
Adammui <adami.yuje@gmail.com>
2024-04-15 04:25:12 +0300

    heals new prefab, they now destroy after 10 seconds(balance later), also heal vignette works too

--- project-steampunk/Assets/Entities/Hp/HealDrop.cs
+++ project-steampunk/Assets/Entities/Hp/HealDrop.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class HealDrop : MonoBehaviour
 {
-    private float _healAmount = 5;
+    public float _healAmount = 5;
+    private float _speed = 30f;
+    public float _healDestroyTime = 10f;
 
+    void Awake()
+    {
+        var m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody.velocity = new Vector3(0, -_speed, 0);
+        Destroy(gameObject, _healDestroyTime);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Healed"+ other.name);
+        
         if (other.gameObject.layer == 7)
-            PickUpHeal(other.gameObject);
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, other.gameObject.transform.position, _speed * Time.deltaTime);
+           
+            PickUpHeal(other.gameObject);  Debug.Log("Healed"+ other.name);
+        }
     }
 
     void PickUpHeal(GameObject player)
