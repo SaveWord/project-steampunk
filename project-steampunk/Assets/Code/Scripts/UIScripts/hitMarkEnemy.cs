03ff9155dea2fd793b9aad3d8915dc0965b5ea08
Adammui <adami.yuje@gmail.com>
2024-05-02 22:54:59 +0300

    crosshairs anims

--- project-steampunk/Assets/Code/Scripts/UIScripts/hitMarkEnemy.cs
+++ project-steampunk/Assets/Code/Scripts/UIScripts/hitMarkEnemy.cs
@@ -8,6 +8,8 @@ public class hitMarkEnemy : MonoBehaviour
     [SerializeField] private GameObject standardCross;
     [SerializeField] private GameObject redCross;
     [SerializeField] private LayerMask enemyLayer;
+
+    [SerializeField] private LayerMask transparentLayer;
     [SerializeField] private WeaponTypeScriptableObj weaponParametrs;
     public float changeRadius;
     void Start()
@@ -30,22 +32,48 @@ public class hitMarkEnemy : MonoBehaviour
             standardCross.gameObject.SetActive(true);
         }
         */
-        if (Physics.SphereCast(Camera.main.transform.position, 2f, Camera.main.transform.forward,
+
+        if (Physics.SphereCast(Camera.main.transform.position, 1f, Camera.main.transform.forward,
                  out RaycastHit hit, weaponParametrs.distanceAndDamages.Last().range, enemyLayer, QueryTriggerInteraction.Ignore))
         {
            for(int i = 0; i <=weaponParametrs.distanceAndDamages.Length-1; i++)
-            {
+           {
                 if (weaponParametrs.distanceAndDamages[i].range > hit.distance)
                 {
                     changeRadius = weaponParametrs.distanceAndDamages[i].radiusRay;
                     break;
                 }
+           }
+        }
+
+        var hits = Physics.SphereCastAll(Camera.main.transform.position, changeRadius, Camera.main.transform.forward, weaponParametrs.distanceAndDamages.Last().range, ~transparentLayer, QueryTriggerInteraction.Ignore);
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward, Color.yellow);
+        if (hits.Length != 0)
+        {
+            var closestHit = hits[0];
+
+            for (int i = 1; i < hits.Length; i++)
+            {
+                if (hits[i].distance < closestHit.distance)
+                {
+                    closestHit = hits[i];
+
+                }
+            }
+            if (closestHit.collider.gameObject.layer == 6)
+            { 
+                redCross.gameObject.SetActive(true);
+                standardCross.gameObject.SetActive(false);
+            }
+            else
+            {
+                redCross.gameObject.SetActive(false);
+                standardCross.gameObject.SetActive(true);
             }
         }
 
-
-
-        if (Physics.SphereCast(Camera.main.transform.position, changeRadius, Camera.main.transform.forward,
+        /*if (Physics.SphereCast(Camera.main.transform.position, changeRadius, Camera.main.transform.forward,
                 out RaycastHit hit_1, weaponParametrs.distanceAndDamages.Last().range, enemyLayer, QueryTriggerInteraction.Ignore))
         {
             redCross.gameObject.SetActive(true);
@@ -55,6 +83,6 @@ public class hitMarkEnemy : MonoBehaviour
         {
             redCross.gameObject.SetActive(false);
             standardCross.gameObject.SetActive(true);
-        }
+        }*/
     }
 }
