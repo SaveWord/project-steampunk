e186214af766559780a6e8dca560ab6d06b227ac
Adammui <adami.yuje@gmail.com>
2024-05-09 01:18:07 +0300

    sound fix

--- project-steampunk/Assets/Code/Scripts/UIScripts/NewUI/MainMenu/UIContinueButton.cs
+++ project-steampunk/Assets/Code/Scripts/UIScripts/NewUI/MainMenu/UIContinueButton.cs
@@ -6,6 +6,9 @@ public class UIContinueButton : MonoBehaviour
 {
     private void Awake()
     {
-        
+        if (GameManagerSingleton.Instance.CheckpointExists())
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
     }
 }
