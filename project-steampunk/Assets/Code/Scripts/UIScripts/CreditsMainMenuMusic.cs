8ea41f5b0a52604a0cd6b6a6b5fa835e8cb8ee86
Denis <denis.kryuchkov.2000@mail.ru>
2024-05-13 02:34:59 +0300

    add texture arm, credits animation, credits change music main menu

---
+++ project-steampunk/Assets/Code/Scripts/UIScripts/CreditsMainMenuMusic.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CreditsMainMenuMusic : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clipMain;
+    public AudioClip clipCredits;
+    private void OnEnable()
+    {
+        audioSource.clip = clipCredits; audioSource.Play();
+    }
+    private void OnDisable()
+    {
+        audioSource.clip = clipMain; audioSource.Play();
+    }
+}
